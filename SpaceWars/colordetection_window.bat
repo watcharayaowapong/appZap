7eac68ae08b8b71ee47e4993288ec3cc6a94640e
ballyeiei <9bally.design@gmail.com>
2021-06-25 14:26:07 +0700

    colordetection

---
+++ SpaceWars/colordetection_window.bat
@@ -0,0 +1,2 @@
+start cmd.exe /k "python color_detection.py"
+start cmd.exe /k "python space_wars.py"
--- SpaceWars/medaipipe_spacewar_window.bat
+++ SpaceWars/medaipipe_spacewar_window.bat
@@ -1,3 +1,3 @@
 start cmd.exe /k "python mouse_control.py"
-start cmd.exe /k "mediapipe_space_war.py"
+start cmd.exe /k "python mediapipe_space_war.py"
 start cmd.exe /k "python space_wars.py"
